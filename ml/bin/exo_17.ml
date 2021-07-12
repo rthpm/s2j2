fd424e916dc7dbf35842c9a0be89aba81bad0337
Denis Zaratan Pasin <zaratan@hey.com>
2021-07-12 16:56:25 +0900

    15 16 17

---
+++ ml/bin/exo_17.ml
@@ -0,0 +1,16 @@
+open Common;;
+
+let build_next_str str =
+  String.sub str 1 (String.length str - 1) ^ "##";;
+
+let rec print_pyr_aux curr_str stage =
+  Printf.printf "%s\n" curr_str;
+  if stage != 1 then
+    print_pyr_aux (build_next_str curr_str) (stage - 1);;
+
+let print_pyr () =
+  let stages = Common.read_times () in
+
+  print_pyr_aux ((String.make (stages - 1) ' ') ^ "#") stages;;
+
+print_pyr();;
