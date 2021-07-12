07bacab20fb8344f61a113968ba1a43bb789ab7f
Denis Zaratan Pasin <zaratan@hey.com>
2021-07-12 16:09:13 +0900

    13 14

---
+++ ml/bin/exo_14.ml
@@ -0,0 +1,20 @@
+open Common;;
+
+let p_email count =
+  if count < 10 then
+    Printf.printf "jean.dupont.0%d@email.fr\n" count
+  else
+    Printf.printf "jean.dupont.%d@email.fr\n" count
+    
+let rec generate_emails_aux count max =
+  if count mod 2 == 0 then
+    p_email count;
+  if count < max then
+    generate_emails_aux (count + 1) max;;
+
+let generate_emails () =
+  let max = Common.read_times() in
+
+  generate_emails_aux 1 max;;
+
+generate_emails ();;
